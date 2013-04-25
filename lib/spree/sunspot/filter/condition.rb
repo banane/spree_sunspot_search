788856e9058619d3ad238bc0aaaa72df6184091b
Kenton Newby <kentonnewby@gmail.com>
2013-04-25 11:17:41 -0400

    updates filter conditions to allow for greater_than_or_equal_to and less_than_or_equal_to conditions on solr queries
      * needed so that we can specify date ranges where one end of
        the range is left blank.
      * prior to this fix, the spree_sunspot_gem would generate
        solr query conditions that excluded endpoint if only one
        endpoint was provided

--- lib/spree/sunspot/filter/condition.rb
+++ lib/spree/sunspot/filter/condition.rb
@@ -11,6 +11,8 @@ module Spree
         LESS_THAN = 2
         BETWEEN = 3
         EQUAL = 4
+        GREATER_THAN_OR_EQUAL_TO = 5
+        LESS_THAN_OR_EQUAL_TO = 6
 
         def multiple?
           value.kindof?(Array)
@@ -26,6 +28,12 @@ module Spree
             elsif range[0] == '*'
               @value = range[1]
               @condition_type = LESS_THAN
+            elsif range[1] == '+'
+              @value = range[0]
+              @condition_type = GREATER_THAN_OR_EQUAL_TO
+            elsif range[0] == '-'
+              @value = range[1]
+              @condition_type = LESS_THAN_OR_EQUAL_TO
             else
               @value = Range.new(range[0], range[1])
               @condition_type = BETWEEN
@@ -40,7 +48,11 @@ module Spree
           case condition_type
             when GREATER_THAN
               "#{value.to_i.to_s},*"
+            when GREATER_THAN_OR_EQUAL_TO
+              "#{value.to_i.to_s},*"
             when LESS_THAN
+              "*,#{value.to_i.to_s}"
+            when LESS_THAN_OR_EQUAL_TO
               "*,#{value.to_i.to_s}"
             when BETWEEN
               "#{value.first.to_i.to_s},#{value.last.to_i.to_s}"
@@ -55,6 +67,10 @@ module Spree
               query.with(source.source).greater_than(value)
             when LESS_THAN
               query.with(source.source).less_than(value)
+            when GREATER_THAN_OR_EQUAL_TO
+              query.with(source.source).greater_than_or_equal_to(value)
+            when LESS_THAN_OR_EQUAL_TO
+              query.with(source.source).less_than_or_equal_to(value)
             when BETWEEN
               query.with(source.source).between(value)
             when EQUAL
